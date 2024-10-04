58a642994d8df2ac82c0dd10157eac45fba7b780
magnu <matterconi@gmail.com>
2024-10-04 19:53:25 +0200

    7c

--- next.config.mjs
+++ next.config.mjs
@@ -4,30 +4,17 @@ import { withSentryConfig } from '@sentry/nextjs';
 const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
-  org: 'mm-d2', // Replace with your Sentry organization slug
-  project: 'javascript-nextjs', // Replace with your Sentry project name
+  org: 'mm-d2',
+  project: 'javascript-nextjs',
   authToken: process.env.SENTRY_AUTH_TOKEN, // Ensure the token is pulled from environment variables
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  reactComponentAnnotation: { enabled: true },
   tunnelRoute: '/monitoring',
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
   automaticVercelMonitors: true,
 });
