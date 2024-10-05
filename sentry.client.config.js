bf83cea7979fe5d5ff81282a65d349313d6c6723
magnu <matterconi@gmail.com>
2024-10-05 19:51:52 +0200

    m2

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,22 +1,33 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
-import { Feedback } from "@sentry/integrations"; // Import Feedback integration explicitly
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
+
   // Add optional integrations for additional features
   integrations: [
-    new Feedback({ // Correctly initialize Feedback
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
       colorScheme: "dark",
     }),
   ],
 
-  // Set Replay sample rates here instead of using the Replay integration
-  replaysSessionSampleRate: 0.1, // Sets the sample rate for sessions to be replayed
-  replaysOnErrorSampleRate: 1.0, // Sets the sample rate for replaying on errors
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-  tracesSampleRate: 1.0, // Set how likely traces are sampled
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: process.env.NODE_ENV !== 'production',
+  debug: false,
 });
