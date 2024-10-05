a43403348216486c76d417e0b8c9f015969a007a
magnu <matterconi@gmail.com>
2024-10-05 12:25:20 +0200

    c

--- instrumentation.js
+++ instrumentation.js
@@ -3,9 +3,7 @@ import * as Sentry from '@sentry/nextjs';
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
+  } else if (process.env.NEXT_RUNTIME === 'edge') {
     await import('./sentry.edge.config');
   }
 }
