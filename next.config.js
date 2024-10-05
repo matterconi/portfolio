65778e77198014c4712d558e3b24ac5d9502968f
magnu <matterconi@gmail.com>
2024-10-05 20:51:12 +0200

    sss

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Add your custom Next.js configurations here if needed
+  };
+  
+  // Export the configuration without Sentry integration
+  export default nextConfig;
+  
